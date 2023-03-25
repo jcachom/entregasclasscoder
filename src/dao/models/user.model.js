5cbd638b3017a7d329065ce317411b0a5500a7a0
jcachom <jcachom10@gmail.com>
2023-03-25 18:45:47 -0500

    Carga

---
+++ src/dao/models/user.model.js
@@ -0,0 +1,28 @@
+let mongoose = require("mongoose");
+let mongoosePaginate = require("mongoose-paginate-v2");
+const modelCollection = "usuarios";
+const modelSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    require: true,
+  },
+  last_name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  dni: Number,
+  birthDate: Date,
+  gender: {
+    type: String,
+    enum: ["M", "F"],
+  },
+});
+modelSchema.plugin(mongoosePaginate);
+const userModel = mongoose.model(modelCollection, modelSchema);
+
+module.exports = userModel;
